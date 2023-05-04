23633c6bbb389b1367319b4b1dcde799f2f3e6bd
KhurshedAlamSifat <khurshedalamsifat@gmail.com>
2023-05-04 20:46:09 +0600

    update 00.034

---
+++ MollaFuelCarService/DAL/Migrations/202305041358219_Picture.cs
@@ -0,0 +1,18 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Picture : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "Picture", c => c.Binary());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "Picture");
+        }
+    }
+}
